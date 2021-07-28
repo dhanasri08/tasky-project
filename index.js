6880b38484d25f75aef437c3e4e7d0042a182e50
Dhana Sri <dhanashree1815@gmail.com>
2021-07-28 20:43:20 +0530

    add contenteditable attribute dynamically

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const taskContainer = document.querySelector(".task__container");
-console.log(taskContainer);
 
 // Global Store
-const globalStore = [];
+let globalStore = [];
 
 const newCard = ({
   id,
@@ -12,16 +11,30 @@ const newCard = ({
   taskType,
 }) => `<div class="col-md-6 col-lg-4" id=${id}>
 <div class="card">
-  <div class="card-header d-flex justify-content-end gap-2"><button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
-    <button type="button" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
+  <div class="card-header d-flex justify-content-end gap-2">
+    <button type="button" id=${id} class="btn btn-outline-success" onclick="editCard.apply(this, arguments)">
+      <i class="fas fa-pencil-alt" id=${id} onclick="editCard.apply(this, arguments)" ></i>
+    </button>
+    <button type="button" id=${id} class="btn btn-outline-danger" onclick="deleteCard.apply(this, arguments)">
+      <i class="fas fa-trash-alt" id=${id} onclick="deleteCard.apply(this, arguments)"></i>
+    </button>
   </div>
-  <img src=${imageUrl} class="card-img-top" alt="...">
+  <img
+    src=${imageUrl}
+    class="card-img-top"
+    alt="..."
+  />
   <div class="card-body">
     <h5 class="card-title">${taskTitle}</h5>
-    <p class="card-text">${taskDescription}</p>
+    <p class="card-text">
+      ${taskDescription}
+    </p>
     <span class="badge bg-primary">${taskType}</span>
   </div>
-  <div class="card-footer text-muted"><button type="button" class="btn btn-outline-primary float-end rounded-pill">Open Task</button>
+  <div class="card-footer text-muted">
+    <button type="button" id=${id} class="btn btn-outline-primary float-end">
+      Open Task
+    </button>
   </div>
 </div>
 </div>`;
@@ -38,6 +51,8 @@ const loadInitialTaskCards = () => {
   });
 };
 
+const updateLocalStorage = () => localStorage.setItem("tasky", JSON.stringify({ cards: globalStore }));
+
 const saveChanges = () => {
     const taskData = {
         id: `${Date.now()}`, 
@@ -53,5 +68,54 @@ const saveChanges = () => {
     taskContainer.insertAdjacentHTML("beforeend", createNewCard);
     globalStore.push(taskData);
 
-    localStorage.setItem("tasky", JSON.stringify{ cards: globalStore });
+    updateLocalStorage();
+};  
+
+const deleteCard = (event) => {
+  // id
+  event = window.event;
+  const targetID = event.target.id;
+  const tagname = event.target.tagName; // BUTTON
+
+  // search the globalStore, remove the object which matches with the id
+  globalStore = globalStore.filter((cardObject) => cardObject.id !== targetID);
+
+  updateLocalStorage();
+
+  // access DOM to remove them
+
+  if (tagname === "BUTTON") {
+    // task__container
+    return taskContainer.removeChild(
+      event.target.parentNode.parentNode.parentNode // col-lg-4
+    );
+  }
+
+  // task__container
+  return taskContainer.removeChild(
+    event.target.parentNode.parentNode.parentNode.parentNode // col-lg-4
+  );
 };
+
+//content-editable
+const editCard = (event) => {
+  event = window.event;
+  const targetID = event.target.id;
+  const tagname = event.target.tagName;
+
+  let parentElement;
+
+  if (tagname === "BUTTON") {
+    parentElement = event.target.parentNode.parentNode;
+  } else {
+    parentElement = event.target.parentNode.parentNode.parentNode;
+  }
+
+  let taskTitle = parentElement.childNodes[5].childNodes[1];
+  let taskDescription = parentElement.childNodes[5].childNodes[3];
+  let taskType = parentElement.childNodes[5].childNodes[5];
+  let submitButton = parentElement.childNodes[7].childNodes[1];
+
+  taskTitle.setAttribute("contenteditable", "true");
+  taskDescription.setAttribute("contenteditable", "true");
+  taskType.setAttribute("contenteditable", "true");
